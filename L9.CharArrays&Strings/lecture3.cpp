c5e5a96512a966f17eeff360f6c1ffe255b4f099
Manish <mannyinwork@gmail.com>
2024-05-04 17:03:11 +0530

    dasf

--- L9.CharArrays&Strings/lecture3.cpp
+++ L9.CharArrays&Strings/lecture3.cpp
@@ -7,15 +7,56 @@ using namespace std;
 
 int main () {
 
-     vector<string> str = {"MMM", "PGM", "GP"};
-     vector<int> travel ({3,10});
-     string mystr;
+     vector<string> garbage = {"G", "P", "GP", "GG"};
+    //  vector<string> garbage = {"G","P","GP","GG"};
+     vector<int> travel ({2,4,3});
+     int pickP=0;
+     int pickG=0;
+     int pickM=0;
 
-    int truckM=0, truckP=0, truckG=0;
-    for (int i = 0; i < str.size(); ++i) {
-        cout << "Index: " << i << ", Value: " << str[i] << endl;
-        // mystr.push_back(str[i]);
+     int travelP=0;
+     int travelG=0;
+     int travelM=0;
+
+     int lastP=0;
+     int lastG=0;
+     int lastM=0;
+
+    for (int i = 0; i < garbage.size(); ++i) {
+        string str = garbage[i];
+        for(int j=0; j<str.length(); j++){
+            char curr = str[j];
+            if(curr == 'P'){
+                pickP++;
+                lastP = i;
+            }
+            else if(curr == 'G'){
+                pickG++;
+                lastG = i;
+            }
+            else if(curr=='M') {
+                pickM++;
+                lastM = i;
+            }
+        }
     }
+
+    for(int i=0; i<lastP; i++){
+        travelP += travel[i];
+    }
+    for(int i=0; i<lastG; i++){
+        travelG += travel[i];
+    }
+    for(int i=0; i<lastM; i++){
+        travelM += travel[i];
+    }
+
+    int ans = (pickP + travelP) + (pickG + travelG) + (pickM + travelM);
+
+    cout<<ans<<endl;
+
+    
+    
 
 
 
