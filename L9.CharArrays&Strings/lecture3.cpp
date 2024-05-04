0f001f1ce4a25735c16c664e582240f7f1e064ce
Manish <mannyinwork@gmail.com>
2024-05-04 16:51:59 +0530

    fsad

--- L9.CharArrays&Strings/lecture3.cpp
+++ L9.CharArrays&Strings/lecture3.cpp
@@ -1,14 +1,36 @@
 #include <iostream>
 #include <string.h>
+#include <vector>
+#include <string>
 
 using namespace std;
 
 int main () {
-    string key = "the quicky brown fox";
-    char start = 'a';
-    char mapping [280] = {0};
-    char ch = 't';
-    mapping[ch]='a';
-    cout<<mapping;
+
+     vector<string> str = {"MMM", "PGM", "GP"};
+     vector<int> travel ({3,10});
+     string mystr;
+
+    int truckM=0, truckP=0, truckG=0;
+    for (int i = 0; i < str.size(); ++i) {
+        cout << "Index: " << i << ", Value: " << str[i] << endl;
+        // mystr.push_back(str[i]);
+    }
+
+
+
+
+
+
+
+
+
+    // string key = "the quicky brown fox";
+    //  cout<<vectorString;
+    // char start = 'a';
+    // char mapping [280] = {0};
+    // char ch = 't';
+    // mapping[ch]='a';
+    // cout<<mapping;
 
 }
