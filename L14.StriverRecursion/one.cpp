c69622cbdc7604244b9d5b9988f0daba1c363181
Manish <mannyinwork@gmail.com>
2024-06-20 11:35:31 +0530

    sdf

--- L14.StriverRecursion/one.cpp
+++ L14.StriverRecursion/one.cpp
@@ -3,23 +3,6 @@
 
 using namespace std;
 
-void findSumsub(int arr[], int ind, int size, int sum, vector<int>&ans){
-    if(ind >= size){
-        ans.push_back(sum);
-        return;
-    }
-
-    findSumsub(arr, ind+1, size, sum + arr[ind], ans);
-
-    findSumsub(arr, ind+1, size, sum, ans);
-}
-
 int main() {
-    int arr[] = {5, 2, 1};
-    vector<int>ans;
-    int sum = 0;
-    findSumsub(arr, 0, 3, sum, ans);
-    for(auto num:ans){
-        cout<<num<<" ";
-    }
+    
 }
