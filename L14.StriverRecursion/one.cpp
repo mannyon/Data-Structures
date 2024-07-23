5de4814035b377951ad6085080816919294a1783
Manish <mannyinwork@gmail.com>
2024-07-23 10:59:00 +0530

    sdfsd

--- L14.StriverRecursion/one.cpp
+++ L14.StriverRecursion/one.cpp
@@ -1,30 +1,8 @@
 #include <iostream>
-#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-void func(int arr[], vector<int>&ans, int size, int ind, int target){
-    if(ind >= size){
-        if(target == 0){
-            for(auto num:ans){
-                cout<<num<<" ";
-            }
-            cout<<endl;
-        }
-        return;
-    }
-    if(target >= arr[ind]){
-        ans.push_back(arr[ind]);
-        func(arr, ans, size, ind, target - arr[ind]);
-        ans.pop_back();
-    }
-    func(arr, ans, size, ind+1, target);
-}
-
 int main() {
-    int arr[] = {2,3,5};
-    vector<int>ans;
-    int target = 8;
-    int size = 3;
-    func(arr, ans, size, 0, target);
+    cout<<"hello world";
 }
