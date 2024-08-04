70ead50810e6a17a7dc1df3a5467921dbbf40009
Manish <mannyinwork@gmail.com>
2024-08-04 12:56:26 +0530

    sdf

--- L13.Week7Assignment/one.cpp
+++ L13.Week7Assignment/one.cpp
@@ -1,38 +1,8 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
 
 using namespace std;
 
-int fibonacci(int n){
-    if(n <= 1){
-        return n;
-    }
-
-    int last = fibonacci(n-1);
-    int slast = fibonacci(n-2);
-    return last + slast;
-}
-
-void func(int arr[], vector<int>&ans, int ind, int size){
-    //base case
-    if(ind >= size){
-        for(auto num:ans){
-            cout<<num<<" ";
-        }
-        cout<<endl;
-        return;
-    }
-
-    //recursive call
-    ans.push_back(arr[ind]);
-    func(arr, ans, ind+1, size);
-    ans.pop_back();
-    func(arr, ans, ind+1, size);
-}
-
 int main() {
-    int arr[] = {3,1,2};
-    vector<int>ans;
-    func(arr, ans, 0, 3);
+    cout<<"Hello World";
 }
