328f1b58f5cd0d156cf2025c9e0f03940b9660b9
Manish <mannyinwork@gmail.com>
2024-09-20 13:55:33 +0530

    sdf

--- L17.Week8Assignment/main.cpp
+++ L17.Week8Assignment/main.cpp
@@ -1,24 +1,41 @@
 #include <iostream>
 #include <vector>
-#include <limits.h>
+#include <cctype>
+#include <string>
 
 using namespace std;
 
-void findMin(int index, int arr[], int n, int &minArr){
-    if(index >= n){
-        return;
+void reverseStr(int i, int j, string &str){
+    if(i >= j) return;
+
+    swap(str[i], str[j]);
+    reverseStr(i+1, j-1, str);
+}
+
+void myrock(int start, int end, string &str){
+    if(start >= end) return;
+
+    char c = str[start];
+    str[start] = str[end];
+    str[end] = c;
+    reverseStr(start+1, end-1, str);
+}
+
+void makeUpper(int ind, string &str){
+
+    if(ind >= str.size()) return;
+
+    if(str[ind] >= 'a' && str[ind] <= 'z'){
+        str[ind] = str[ind] - 32;
     }
 
-    if(arr[index] < minArr){
-        minArr = arr[index];
-    }
-    findMin(index+1, arr, n, minArr);
-
+    makeUpper(ind+1, str);
 }
 
 int main() {
-    int arr[] = {7,2,4,5,6};
-    int minArr = INT_MAX;
-    findMin(0, arr, 5, minArr);
-    cout<<minArr<<endl;
+    string str = "manish";
+
+    makeUpper(0, str);
+
+    cout<<str<<endl;
 }
