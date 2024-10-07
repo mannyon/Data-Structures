d67db631a47e8ef9da1905589a9e93b04c65d40a
Manish <mannyinwork@gmail.com>
2024-10-07 12:40:30 +0530

    sdf

--- L17.Week8Assignment/main.cpp
+++ L17.Week8Assignment/main.cpp
@@ -3,6 +3,27 @@
 
 using namespace std;
 
+void findNBitBinary(string &str, int zero, int one, int n){
+    if(str.size() == n){
+        cout<<str<<endl;
+        return;
+    }
+
+    if(str.size() != 0){
+        str.push_back('0');
+        findNBitBinary(str, zero+1, one, n);
+        str.pop_back();
+    } 
+
+    str.push_back('1');
+    findNBitBinary(str, zero, one+1, n);
+    str.pop_back();
+
+   
+}
+
 int main() {
-    cout<<"Hello World";
+    int n = 5;
+    string str = "";
+    findNBitBinary(str, 0, 0, n);
 }
